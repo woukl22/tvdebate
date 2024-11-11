de300c01fa50b74deaad8335dfb8db6228c736ae
woukl22 <woukl22@gmail.com>
2024-11-12 02:28:01 +0900

    Chore: Eslint & prettier setting

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+});
