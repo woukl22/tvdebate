294c4cacbe150967daf45e8fb98f9b4b9fa92432
woukl22 <woukl22@gmail.com>
2024-11-12 22:39:57 +0900

    Chore: Structure setting

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import { React, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
